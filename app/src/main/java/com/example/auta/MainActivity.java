207df7aeeceb71b6197851d21d1eb4ae18bb2588
Karol3ke36 <karolpucyk9@gmail.com>
2026-05-22 09:22:59 +0200

    koniec na dzis

--- app/src/main/java/com/example/auta/MainActivity.java
+++ app/src/main/java/com/example/auta/MainActivity.java
@@ -1,6 +1,12 @@
 package com.example.auta;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.RadioGroup;
+import android.widget.SeekBar;
+import android.widget.Spinner;
+import android.widget.Switch;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +15,11 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    Spinner marka;
+    ListView model, wynik;
+    Switch aSwitch;
+    SeekBar rocznik;
+    RadioGroup kolor, wlasciciel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +31,14 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        marka = findViewById(R.id.marka);
+        model = findViewById(R.id.model);
+        wynik = findViewById(R.id.wyniki);
+        aSwitch = findViewById(R.id.switch1);
+        rocznik = findViewById(R.id.seekBar);
+        kolor = findViewById(R.id.kolor);
+        wlasciciel = findViewById(R.id.wlasciciel);
+        ArrayAdapter adapter = new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_list_item_1);
+        model.setAdapter(adapter);
     }
 }
